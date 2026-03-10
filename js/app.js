d1d331e532940145ff303bf5069320533e8eb095
amansgz <amansgz.ar@gmail.com>
2026-03-10 14:00:10 -0300

    feat(nav): implement close menu when clicking a link

--- js/app.js
+++ js/app.js
@@ -5,3 +5,10 @@ navToggle.addEventListener("click", () => {
   const isExpanded = navToggle.getAttribute("aria-expanded") === "true";
   navToggle.setAttribute("aria-expanded", !isExpanded);
 });
+
+// Close menu when clicking a link
+document.querySelectorAll(".nav__link").forEach((link) => {
+  link.addEventListener("click", () => {
+    navToggle.setAttribute("aria-expanded", "false");
+  });
+});
